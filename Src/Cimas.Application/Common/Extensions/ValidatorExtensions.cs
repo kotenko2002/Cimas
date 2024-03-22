5a949b6d55223fc0c572e2f19708e700a6c34b11
kotenko2002 <kotenko2002@gmail.com>
2024-03-22 11:36:54 +0200

    Unified and placed validation rules in a separate class

---
+++ Src/Cimas.Application/Common/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Cimas.Application.Common.Extensions
+{
+    public static class ValidatorExtensions
+    {
+        public static IRuleBuilderOptions<T, IEnumerable<TElement>> MustHaveUniqueIds<T, TElement, TId>(
+            this IRuleBuilder<T, IEnumerable<TElement>> ruleBuilder, Func<TElement, TId> idSelector)
+            where TId : notnull
+        {
+            return ruleBuilder.Must(list => list.Select(idSelector).Distinct().Count() == list.Count())
+                .WithMessage("All Ids must be unique");
+        }
+
+        public static IRuleBuilderOptions<T, IEnumerable<TElement>> MustBeValidEnum<T, TElement, TEnum>(
+            this IRuleBuilder<T, IEnumerable<TElement>> ruleBuilder, Func<TElement, TEnum> enumSelector)
+            where TEnum : struct, Enum
+        {
+            IEnumerable<TEnum> enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+            IEnumerable<string> enumNamesWithValues = enumValues.Select(e => $"{Convert.ToInt32(e)} - {Enum.GetName(typeof(TEnum), e)}");
+            string enumDescriptions = string.Join(", ", enumNamesWithValues);
+
+            return ruleBuilder.Must(list => list.All(item => Enum.IsDefined(typeof(TEnum), enumSelector(item))))
+                .WithMessage($"All enum values must be valid. Valid values are: {enumDescriptions}");
+        }
+    }
+}
--- Src/Cimas.Application/Features/Tickets/Commands/CreateTickets/CreateTicketsValidator.cs
+++ Src/Cimas.Application/Features/Tickets/Commands/CreateTickets/CreateTicketsValidator.cs
@@ -1,6 +1,5 @@
-﻿using Cimas.Domain.Entities.Tickets;
+﻿using Cimas.Application.Common.Extensions;
 using FluentValidation;
-using FluentValidation.Results;
 
 namespace Cimas.Application.Features.Tickets.Commands.CreateTickets
 {
@@ -10,34 +9,8 @@ namespace Cimas.Application.Features.Tickets.Commands.CreateTickets
         {
             RuleFor(x => x.Tickets)
                 .NotEmpty()
-                .Must(Tickets => Tickets.Count > 0)
-                .WithMessage("'Tickets' must contain at least 1 tickets")
-                .Must(HaveUniqueIds)
-                .WithMessage("All seat Ids must be unique")
-                .DependentRules(() =>
-                {
-                    RuleFor(x => x.Tickets)
-                    .Custom((seats, context) => AreValidSeats(seats, context));
-                });
+                .MustHaveUniqueIds(ticket => ticket.SeatId)
+                .MustBeValidEnum(ticket => ticket.Status);
         }
-
-        private bool HaveUniqueIds(List<CreateTicketModel> seats)
-           => seats.DistinctBy(seat => seat.SeatId).Count() == seats.Count;
-
-        private bool AreValidSeats(List<CreateTicketModel> seats, ValidationContext<CreateTicketsCommand> context)
-        {
-            var invalidSeats = seats.Where(seat => !IsValiSeat(seat)).ToList();
-
-            foreach (var invalidSeat in invalidSeats)
-            {
-                context.AddFailure(new ValidationFailure(
-                    "Seats",
-                    $"Seat with Id: {invalidSeat.SeatId} is not valid. Please ensure the seat has a valid Id, and has a valid status"));
-            }
-
-            return !invalidSeats.Any();
-        }
-        private bool IsValiSeat(CreateTicketModel ticket)
-            => ticket.SeatId != Guid.Empty && Enum.IsDefined(typeof(TicketStatus), ticket.Status);
     }
 }
